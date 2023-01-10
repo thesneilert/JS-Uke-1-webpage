542ad88af323e203fb3675e3f5704a2f180e26e6
thesneilert <eilert.gaming@outlook.com>
2023-01-10 12:15:49 +0100

    changed button to gradient
    darkmode/daymode function on font

--- colormodechange.js
+++ colormodechange.js
@@ -5,11 +5,13 @@ function changecolormode()
     if (nightmode == false) {
         document.getElementById("background").style.backgroundColor = "#1c1c1c"
         document.getElementById("list").style.color = "#cccccc"
+        document.getElementById("button-changecolor").style.color = "#e4e4e4"
         nightmode = true
     }
     else {
         document.getElementById("background").style.backgroundColor = "white"
         document.getElementById("list").style.color = "black"
+        document.getElementById("button-changecolor").style.color = "white"
         nightmode = false
     }
 }
