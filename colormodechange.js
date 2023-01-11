422fe1e26617549d5de6b132ba40a98a01590a3f
thesneilert <eilert.gaming@outlook.com>
2023-01-11 15:10:38 +0100

    rewriting site from scratch
    making it more clean, and easier for later

--- colormodechange.js
+++ colormodechange.js
@@ -1,15 +1,15 @@
-//dark&day mode button function
+//darkday mode function
 var nightmode = true;
-function changecolormode() {
+function changecolormode(daydarkmode) {
   if (nightmode == false) {
     document.getElementById("background").style.backgroundColor = "#1c1c1c";
     document.getElementById("list").style.color = "#cccccc";
-    document.getElementById("button-changecolor").style.color = "#e4e4e4";
+    document.getElementById('button-changecolor').style.color = "#e4e4e4";
     nightmode = true;
   } else {
     document.getElementById("background").style.backgroundColor = "white";
     document.getElementById("list").style.color = "black";
-    document.getElementById("button-changecolor").style.color = "white";
+    document.getElementById('button-changecolor').style.color = "white";
     nightmode = false;
   }
 }
