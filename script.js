7a0e923d0375b15982122864a960ef1fb6f960cc
thesneilert <eilert.gaming@outlook.com>
2023-01-12 08:45:06 +0100

    fixed buttons and added funtion

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //darkmode mode funksjon
-
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 
-toggle.addEventListener('click', function(){
+toggleDark.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high')){
         body.style.background = 'white';
@@ -19,18 +18,43 @@ toggle.addEventListener('click', function(){
 
 
 //like button
+const heart = document.getElementById('clickHeart');
+let isHeartFilled = false;
 
-const toggle = document.getElementById('toggleHeart');
-const body = document.querySelector('body');
-
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi bi-heart-fill');
-    if(this.classList.toggle('bi bi-heart')){
-        body.style.color = 'black';
-        body.style.transition = '2s';
-        
-    }else (this.classList.toggle('bi bi-heart-fill');{
-        body.style.color = '#ff652f';
-        body.style.transition = '2s';
+heart.addEventListener('click', function() {
+    if (isHeartFilled) {
+        this.classList.remove('bi-heart-fill');
+        this.classList.add('bi-heart');
+        isHeartFilled = false;
+    } else {
+        this.classList.remove('bi-heart');
+        this.classList.add('bi-heart-fill');
+        isHeartFilled = true;
     }
 });
+
+
+//github dropdown
+var toggleGithub = document.getElementById("toggleGithub");
+  var dropdown = document.getElementById("dropdown");
+  
+  toggleGithub.addEventListener("click", function() {
+    if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
+    } else {
+      dropdown.style.display = "block";
+    }
+  });
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.githubKnapp')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.style.display === "block") {
+          openDropdown.style.display = "none";
+        }
+      }
+    }
+  }
